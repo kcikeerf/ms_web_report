252a5cddfd23902aee18a9b68bdd9f6b73d3e067
Jinlong Zhang <Jinlong Zhang>
2017-07-18 17:12:45 +0800

    优化zxview逻辑

--- src/zxView/component/LeftNav/UserList.js
+++ src/zxView/component/LeftNav/UserList.js
@@ -2,25 +2,69 @@ import React from 'react';
 import PropTypes from 'prop-types'; // ES6
 import $ from 'jquery';
 
-// let config = require('zx-const')[process.env.NODE_ENV];
+import removeCookie from 'zx-misc/removeCookie';
+
+let config = require('zx-const')[process.env.NODE_ENV];
 
 export default class UserList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            bindedUserList: null
+        }
+    }
+
     componentDidMount() {
-        $('#zxUserSelect').change(function(){
-            let userName = $('#zxUserSelect').find(':selected').val();
-            let userRole = $('#zxUserSelect').find(':selected').attr('data-user-role');
-            let userDisplayName = $('#zxUserSelect').find(':selected').attr('data-user-display-name');
-            let selectedAccessToken = $('#zxUserSelect').find(':selected').attr('data-access-token');
-            this.props.handleTestList(selectedAccessToken, userName, userRole, userDisplayName);
+        this.setState({
+            bindedUserList: [this.props.mainUser]
+        });
+        this.handleBindedUserList(this.props.mainAccessToken);
+
+        let selector = $('#zxUserSelect');
+        selector.change(function(){
+            let selected = selector.find(':selected');
+            let selectedAccessToken = selected.attr('data-access-token');
+            let selectedUserName = selected.val();
+            let selectedUserRole = selected.attr('data-user-role');
+            let selectedUserDisplayName = selected.attr('data-user-display-name');
+            this.props.handleDashboardUserInfo(selectedAccessToken, selectedUserName, selectedUserRole, selectedUserDisplayName);
+        }.bind(this));
+    }
+
+    handleBindedUserList(mainAccessToken) {
+        let bindedUserListApi = config.API_DOMAIN + config.API_GET_BINDED_USERS;
+        let bindedUserListData = {
+            access_token: mainAccessToken
+        };
+        let bindedUserListPromise = $.post(bindedUserListApi, bindedUserListData);
+        bindedUserListPromise.done(function (response) {
+            this.setState({
+                bindedUserList: [this.props.mainUser, ...response]
+            });
+        }.bind(this));
+        bindedUserListPromise.fail(function (errorResponse) {
+            let repsonseJSON = errorResponse.responseJSON;
+            if (repsonseJSON) {
+                let error = repsonseJSON.error;
+                if (error === 'Access Token 已过期') {
+                    removeCookie('access_token');
+                    this.setState({
+                        selectedAccessToken: null,
+                        mainAccessToken: null
+                    });
+                    this.context.router.push('/login');
+                }
+            }
+            else {
+
+            }
         }.bind(this));
     }
 
     render() {
-        let bindedUserList = this.props.bindedUserList;
+        let bindedUserList = this.state.bindedUserList;
         let contentUserList;
-        // let contentUserSelectTitle = <div className="zx-list-subtitle">用户加载中...</div>;
         if (bindedUserList) {
-            // contentUserSelectTitle = null;
             contentUserList = bindedUserList.map((bindedUser, index) => {
                 return <UserItem
                     key={index}
@@ -29,7 +73,6 @@ export default class UserList extends React.Component {
                     userDisplayName={bindedUser.name}
                     userRole={bindedUser.role}
                     selectedAccessToken={bindedUser.oauth.access_token}
-                    handleTestList={this.props.handleTestList.bind(this)}
                 />
             });
         }
@@ -48,7 +91,7 @@ export default class UserList extends React.Component {
 }
 
 UserList.contextTypes = {
-    handleTestList: PropTypes.func
+    handleDashboardUserInfo: PropTypes.func
 };
 
 class UserItem extends React.Component {
@@ -56,17 +99,9 @@ class UserItem extends React.Component {
         $(document).ready(function() {
             $('select').material_select();
         });
-
-        if (this.props.id === 0) {
-            this.props.handleTestList(this.props.selectedAccessToken, this.props.userName, this.props.userRole, this.props.userDisplayName);
-        }
     }
 
     render() {
-        // let selected = null;
-        // if (this.props.id === 0) {
-        //     selected = 'selected';
-        // }
         return (
             <option
                 value={this.props.userName}
@@ -81,5 +116,4 @@ class UserItem extends React.Component {
 }
 
 UserItem.contextTypes = {
-    handleTestList: PropTypes.func
 };
