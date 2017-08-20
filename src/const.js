84c9053332edd88ca090e653130381cab99c677e
Jinlong Zhang <Jinlong Zhang>
2017-08-20 19:42:23 +0800

    修改配置

--- src/const.js
+++ src/const.js
@@ -189,7 +189,7 @@ let production = {
 
     API_LOGIN_STATE:'prod',
     // 服务器地址
-    API_DOMAIN: 'http://59.110.7.209:4502',
+    API_DOMAIN: 'http://www.k12ke.com',
     /*
      ================
      微信登录
